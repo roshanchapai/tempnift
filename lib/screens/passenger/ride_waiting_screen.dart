26a6b3f5849e7b63472e9ef4bcc35a2c122da2b7
Roshan Chapai <roshanchapai@gmail.com>
2025-04-10 23:03:45 +0545

    removed unused dependency fixed an error in rider waiting screen

--- lib/screens/passenger/ride_waiting_screen.dart
+++ lib/screens/passenger/ride_waiting_screen.dart
@@ -5,7 +5,6 @@ import 'package:nift_final/models/user_model.dart';
 import 'package:nift_final/screens/passenger/ongoing_ride_screen.dart';
 import 'package:nift_final/services/ride_service.dart';
 import 'package:nift_final/utils/constants.dart';
-import 'package:lottie/lottie.dart';
 import 'package:nift_final/screens/map_screen.dart';
 import 'package:nift_final/screens/home_screen.dart';
 import 'package:nift_final/widgets/nift_logo.dart';
@@ -29,10 +28,7 @@ class _RideWaitingScreenState extends State<RideWaitingScreen> with SingleTicker
   StreamSubscription<RideRequest?>? _rideStatusSubscription;
   bool _isLoading = false;
   late AnimationController _animationController;
-  late Animation<Offset> _carAnimation;
-  late Animation<double> _roadMarkingAnimation;
-  late Animation<double> _scaleAnimation;
-  late Animation<Color?> _skyColorAnimation;
+  List<Animation<double>> _dotAnimations = [];
   String _statusMessage = 'Waiting for a rider to accept your request...';
   String? _acceptedRiderId;
   UserModel? _acceptedRider;
@@ -42,49 +38,30 @@ class _RideWaitingScreenState extends State<RideWaitingScreen> with SingleTicker
     super.initState();
     _animationController = AnimationController(
       vsync: this,
-      duration: const Duration(seconds: 2),
-    )..repeat(reverse: false);
+      duration: const Duration(milliseconds: 1200),
+    )..repeat();
     
-    // Car movement animation
-    _carAnimation = Tween<Offset>(
-      begin: const Offset(-0.2, 0),
-      end: const Offset(0.2, 0),
-    ).animate(CurvedAnimation(
-      parent: _animationController,
-      curve: Curves.easeInOut,
-    ));
-    
-    // Road marking animation
-    _roadMarkingAnimation = Tween<double>(
-      begin: -300,
-      end: MediaQuery.of(context).size.width,
-    ).animate(CurvedAnimation(
-      parent: _animationController,
-      curve: Curves.linear,
-    ));
-    
-    // Car bounce animation
-    _scaleAnimation = TweenSequence<double>([
-      TweenSequenceItem(
-        tween: Tween<double>(begin: 1.0, end: 1.05)
-            .chain(CurveTween(curve: Curves.easeInOut)),
-        weight: 50,
-      ),
-      TweenSequenceItem(
-        tween: Tween<double>(begin: 1.05, end: 1.0)
-            .chain(CurveTween(curve: Curves.easeInOut)),
-        weight: 50,
-      ),
-    ]).animate(_animationController);
-    
-    // Sky color animation for day-night cycle effect
-    _skyColorAnimation = ColorTween(
-      begin: AppColors.primaryColor.withOpacity(0.1),
-      end: AppColors.primaryColor.withOpacity(0.3),
-    ).animate(CurvedAnimation(
-      parent: _animationController,
-      curve: Curves.easeInOut,
-    ));
+    // Create staggered animations for each dot
+    for (int i = 0; i < 6; i++) {
+      final delay = i * 0.15;
+      final begin = 0.0 + delay;
+      final end = 1.0 + delay;
+      
+      _dotAnimations.add(
+        TweenSequence<double>([
+          TweenSequenceItem(
+            tween: Tween<double>(begin: 0, end: -10)
+                .chain(CurveTween(curve: Interval(begin.clamp(0, 1), (begin + 0.5).clamp(0, 1), curve: Curves.easeOut))),
+            weight: 50,
+          ),
+          TweenSequenceItem(
+            tween: Tween<double>(begin: -10, end: 0)
+                .chain(CurveTween(curve: Interval((begin + 0.5).clamp(0, 1), (end).clamp(0, 1), curve: Curves.easeIn))),
+            weight: 50,
+          ),
+        ]).animate(_animationController),
+      );
+    }
     
     _startListeningToRideStatus();
   }
@@ -296,190 +273,31 @@ class _RideWaitingScreenState extends State<RideWaitingScreen> with SingleTicker
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    // Advanced animated road with car
-                    Container(
+                    // Simple bouncing dots animation
+                    SizedBox(
                       width: double.infinity,
-                      height: 220,
+                      height: 120,
                       child: AnimatedBuilder(
                         animation: _animationController,
                         builder: (context, child) {
-                          return Stack(
-                            alignment: Alignment.center,
-                            children: [
-                              // Sky background with animated color
-                              Container(
-                                width: double.infinity,
-                                height: 220,
-                                decoration: BoxDecoration(
-                                  gradient: LinearGradient(
-                                    begin: Alignment.topCenter,
-                                    end: Alignment.bottomCenter,
-                                    colors: [
-                                      _skyColorAnimation.value ?? AppColors.primaryColor.withOpacity(0.2),
-                                      Colors.white,
-                                    ],
-                                  ),
-                                ),
-                              ),
-                              
-                              // Buildings or city skyline in background
-                              Positioned(
-                                bottom: 95,
-                                left: 0,
-                                right: 0,
-                                child: Row(
-                                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                                  children: List.generate(6, (index) {
-                                    final height = 20.0 + (index % 3) * 15.0;
-                                    return Container(
-                                      margin: const EdgeInsets.symmetric(horizontal: 4),
-                                      width: 40,
-                                      height: height,
-                                      decoration: BoxDecoration(
-                                        color: Colors.blueGrey.withOpacity(0.7),
-                                        borderRadius: const BorderRadius.only(
-                                          topLeft: Radius.circular(4),
-                                          topRight: Radius.circular(4),
-                                        ),
-                                      ),
-                                      child: Row(
-                                        mainAxisAlignment: MainAxisAlignment.center,
-                                        children: List.generate(2, (windowIndex) {
-                                          return Container(
-                                            margin: const EdgeInsets.all(2),
-                                            width: 5,
-                                            height: 5,
-                                            color: Colors.yellow.withOpacity(0.7),
-                                          );
-                                        }),
-                                      ),
-                                    );
-                                  }),
-                                ),
-                              ),
-                              
-                              // Road
-                              Positioned(
-                                bottom: 50,
-                                left: 0,
-                                right: 0,
-                                child: Container(
-                                  height: 40,
-                                  color: Colors.blueGrey.shade800,
-                                ),
-                              ),
-                              
-                              // Road markings (animated)
-                              Positioned(
-                                bottom: 68,
-                                left: _roadMarkingAnimation.value - 600, // Starting offscreen
-                                child: Row(
-                                  mainAxisAlignment: MainAxisAlignment.start,
-                                  children: List.generate(10, (index) => 
-                                    Container(
-                                      margin: const EdgeInsets.only(right: 20),
-                                      width: 40,
-                                      height: 5,
-                                      decoration: BoxDecoration(
-                                        color: Colors.white,
-                                        borderRadius: BorderRadius.circular(2),
-                                      ),
-                                    )
-                                  ),
-                                ),
-                              ),
-                              
-                              // Car with subtle bounce animation
-                              Positioned(
-                                bottom: 75,
-                                child: SlideTransition(
-                                  position: _carAnimation,
-                                  child: ScaleTransition(
-                                    scale: _scaleAnimation,
-                                    child: SizedBox(
-                                      width: 90,
-                                      height: 35,
-                                      child: Stack(
-                                        children: [
-                                          // Car body
-                                          Container(
-                                            width: 80,
-                                            height: 25,
-                                            decoration: BoxDecoration(
-                                              color: AppColors.primaryColor,
-                                              borderRadius: BorderRadius.circular(10),
-                                              boxShadow: [
-                                                BoxShadow(
-                                                  color: Colors.black.withOpacity(0.2),
-                                                  blurRadius: 5,
-                                                  offset: const Offset(0, 3),
-                                                ),
-                                              ],
-                                            ),
-                                          ),
-                                          
-                                          // Car top/cabin
-                                          Positioned(
-                                            top: 0,
-                                            left: 20,
-                                            child: Container(
-                                              width: 40,
-                                              height: 15,
-                                              decoration: BoxDecoration(
-                                                color: AppColors.primaryColor.withOpacity(0.8),
-                                                borderRadius: BorderRadius.circular(5),
-                                              ),
-                                            ),
-                                          ),
-                                          
-                                          // Wheels
-                                          Positioned(
-                                            bottom: 0,
-                                            left: 15,
-                                            child: Container(
-                                              width: 15,
-                                              height: 15,
-                                              decoration: BoxDecoration(
-                                                color: Colors.black,
-                                                shape: BoxShape.circle,
-                                                border: Border.all(color: Colors.white, width: 2),
-                                              ),
-                                            ),
-                                          ),
-                                          Positioned(
-                                            bottom: 0,
-                                            right: 15,
-                                            child: Container(
-                                              width: 15,
-                                              height: 15,
-                                              decoration: BoxDecoration(
-                                                color: Colors.black,
-                                                shape: BoxShape.circle,
-                                                border: Border.all(color: Colors.white, width: 2),
-                                              ),
-                                            ),
-                                          ),
-                                          
-                                          // Headlights
-                                          Positioned(
-                                            top: 10,
-                                            right: 0,
-                                            child: Container(
-                                              width: 8,
-                                              height: 8,
-                                              decoration: BoxDecoration(
-                                                color: Colors.yellow,
-                                                borderRadius: BorderRadius.circular(2),
-                                              ),
-                                            ),
-                                          ),
-                                        ],
-                                      ),
+                          return Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: List.generate(6, (index) {
+                              return Container(
+                                padding: const EdgeInsets.symmetric(horizontal: 8),
+                                child: Transform.translate(
+                                  offset: Offset(0, _dotAnimations[index].value),
+                                  child: Container(
+                                    width: 12,
+                                    height: 12,
+                                    decoration: BoxDecoration(
+                                      color: AppColors.primaryColor,
+                                      shape: BoxShape.circle,
                                     ),
                                   ),
                                 ),
-                              ),
-                            ],
+                              );
+                            }),
                           );
                         },
                       ),
